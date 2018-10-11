7704ed1f3a37b0af6d9efc1648eaae81590c64e5
Loafus <eth.fish@gmail.com>
2018-10-10 17:53:38 -0700

    minor changes

---
+++ main.js
@@ -0,0 +1,13 @@
+// function sendMail() {
+//     var link = "mailto:eth.fish@gmail.com"
+//              + "?"
+//              + "&subject=" + escape("This is my subject")
+//              + "&body=" + escape(document.getElementById('message').value)
+//     ;
+
+//     window.location.href = link;
+// }
+
+// document.getElementById("submit-btn").addEventListener("click", function() {
+//     sendMail(); return false
+// });
